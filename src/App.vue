25e31a41a50b5b241706daf5276d6f8ebfa0a9aa
tr0zanwow <pranil.rathod93@gmail.com>
2020-05-04 21:21:27 +0530

    Added# Mobile layout

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,37 @@
 			/>
 
 			<v-toolbar-title class="white--text">
-				GeoSpoc <small> - Test Project</small></v-toolbar-title
-			>
+				GeoSpoc
+			</v-toolbar-title>
+
 			<v-spacer></v-spacer>
-			<v-btn class="d-none d-lg-flex d-xl-none" to="/dashboard" text small dark=""
+			<v-btn class="d-none d-xl-flex d-none d-lg-flex d-xl-none d-none d-md-flex d-lg-none d-none d-sm-flex d-md-none" to="/dashboard" text small dark=""
 				><v-icon size="26" left>dashboard</v-icon>Dashboard</v-btn
 			>
-			<v-btn class="d-none d-lg-flex d-xl-none" to="/about" text small dark=""
-				><v-icon size="26" left>info_outline</v-icon>sssAbout</v-btn
+			<v-btn class="d-none d-xl-flex d-none d-lg-flex d-xl-none d-none d-md-flex d-lg-none d-none d-sm-flex d-md-none" to="/about" text small dark=""
+				><v-icon size="26" left>info_outline</v-icon>About</v-btn
 			>
-			<div class="d-flex d-sm-none">
-				<v-btn to="/about" icon dark=""
-				><v-icon large="">account</v-icon></v-btn
+
+			<v-btn class="d-flex d-sm-none" to="/dashboard" icon dark=""
+				><v-icon >dashboard</v-icon></v-btn
 			>
-			</div>
-			
+
+			<v-btn class="d-flex d-sm-none" to="/about" icon dark=""
+				><v-icon >info_outline</v-icon></v-btn
+			>
 		</v-app-bar>
 
-		<v-content>
+		<v-content v-if="!isMobile()">
 			<v-container class="fill-height" fluid>
+				<transition name="fade">
+					<keep-alive>
+						<router-view></router-view>
+					</keep-alive>
+				</transition>
+			</v-container>
+		</v-content>
+		<v-content v-else>
+			<v-container class="fill-height" style="height: calc(100% - 53px)" fluid>
 				<transition name="fade">
 					<keep-alive>
 						<router-view></router-view>
@@ -40,13 +52,24 @@
 	</v-app>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+</style>
 
 <script>
 export default {
 	name: "App",
 	props: {
 		source: String
+	},
+	methods:{
+		isMobile() {
+			if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+				return true
+			} else {
+				return false
+			}
+			},
 	},
 	components: {},
 
